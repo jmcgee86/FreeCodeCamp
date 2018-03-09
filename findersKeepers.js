db852127f514ea4e9bfc1653e59f70eb9bf34501
Jim McGee <jmcgee86@gmail.com>
2018-03-09 01:29:15 +0000

    add better solution for finders keepers

--- findersKeepers.js
+++ findersKeepers.js
@@ -1,9 +1,14 @@
-function findElement(arr, func) {
-  var passes = 0;
-  for(var i=0; i<=arr.length; i++){
-     passes = (arr.filter(func))[0];
-    }
-return passes;
-}
+// function findElement(arr, func) {
+//   var passes = 0;
+//   for(var i=0; i<=arr.length; i++){
+//     passes = (arr.filter(func))[0];
+//     }
+// return passes;
+// }
 
-findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
+// findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
+
+function findElement(arr, func) {
+  return arr.filter(func)[0];
+}
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
