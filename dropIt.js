51a225ead3e55ad5ab31198902fef5ba3730642b
Jim McGee <jmcgee86@gmail.com>
2018-03-09 01:44:09 +0000

    better solution for drop it

--- dropIt.js
+++ dropIt.js
@@ -1,21 +1,33 @@
+// function dropElements(arr, func) {
+//   // Drop them elements.
+//   var fail = [];
+//   for (var i = 0; i<arr.length; i++){
+//     if (!func(arr[i])){
+//       fail.push(arr[i]);
+//     }
+//   }
+//   for (var n=0; n<arr.length; n++){
+//      if (fail.length == arr.length){
+//       arr = [];
+//      }
+//      else if (func(arr[n])){
+//       arr.splice(0,n);
+//       break;
+//     }
+//   } 
+//   return arr;
+// }
+
+// dropElements([1, 2, 3, 4], function(n) {return n >= 3;});
+
 function dropElements(arr, func) {
-  // Drop them elements.
-  var fail = [];
   for (var i = 0; i<arr.length; i++){
-    if (!func(arr[i])){
-      fail.push(arr[i]);
+    if (func(arr[i])){
+      return arr.slice(i);
     }
   }
-   for (var n=0; n<arr.length; n++){
-     if (fail.length == arr.length){
-       arr = [];
-     }
-     else if (func(arr[n])){
-      arr.splice(0,n);
-      break;
-    }
-   } 
-  return arr;
+ return []; 
 }
+
 
 dropElements([1, 2, 3, 4], function(n) {return n >= 3;});
