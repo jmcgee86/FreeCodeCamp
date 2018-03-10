a04dae62fb120539c1b39eb15fa3b93f416c4fb4
Jim McGee <jmcgee86@gmail.com>
2018-03-10 15:21:52 +0000

    better solution for everything be true

--- everythingBeTrue.js
+++ everythingBeTrue.js
@@ -1,20 +1,44 @@
-function truthCheck(collection, pre) {
-  // Is everyone being true?
-  var trueArray = [];
-  var falseArray = [];
-  for (var i = 0; i<collection.length; i++){
-  if(collection[i][pre]){
-    trueArray.push(collection[i]);
-  }else{
-    falseArray.push(collection[i]);
-  }
-  }
-  if (falseArray.length>0){
-    return false;
-  }else{
-    return true;
-  }
+// function truthCheck(collection, pre) {
+//   // Is everyone being true?
+//   var trueArray = [];
+//   var falseArray = [];
+//   for (var i = 0; i<collection.length; i++){
+//   if(collection[i][pre]){
+//     trueArray.push(collection[i]);
+//   }else{
+//     falseArray.push(collection[i]);
+//   }
+//   }
+//   if (falseArray.length>0){
+//     return false;
+//   }else{
+//     return true;
+//   }
   
-}
+// }
 
+// truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat");
+
+// function truthCheck(collection, pre) {
+// var detector; 
+//   for (var i = 0; i<collection.length; i++){
+//   if(collection[i][pre]){
+//     detector = true;
+//   }else{
+//     detector = false;
+//     return detector;
+//   }
+// }
+//   return detector;
+// }
+// truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat");
+
+function truthCheck(collection, pre) {
+   for (var i = 0; i<collection.length; i++){
+  if(!collection[i][pre]){
+  return false;
+  }
+}
+  return true;
+}
 truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat");
